8d94435f19805b830fb913f0e144cc22218c9ad7
bbantier <65014410+bbantier@users.noreply.github.com>
2025-06-02 09:18:52 +0200

    feat: create card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   return (
     <>
+      <Card id={25} />
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function Card({id}) {
+  const [data, setData] = useState({});
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+  const imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+
+  const fetchData = () => {
+    fetch(url + id)
+      .then(response => response.json())
+      .then(data => setData(data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  })
+
+  return (
+    <div className="grid">
+      <img src={imgUrl + id + ".png"}></img>
+      <h2>{data.name}</h2>
+    </div>
+  )
+}
